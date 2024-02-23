7b17d7808e49534d76ae48a914fc8dd09d00df9e
Kavalskiy <vanyamalisheff@yandex.ru>
2024-02-23 16:48:01 +0300

    Added server to backend

--- src/backend/api.js
+++ src/backend/api.js
@@ -1,3 +1,9 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+const PORT = 3000;
+
 const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=55.991893%2C37.214390';
 const options = {
 	method: 'GET',
@@ -7,10 +13,17 @@ const options = {
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+app.get('/api/weather-info', async (req, res) => {
+	try {
+		const response = await fetch(url, options);
+		const result = await response.text();
+		res.send(result);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('ERRRRRRRRRROR');
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Start on port: ${PORT}`);
+});
