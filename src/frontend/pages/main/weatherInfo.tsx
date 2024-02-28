298543af46cdeee37138289be50b5d2fc49d91cd
Kavalskiy <vanyamalisheff@yandex.ru>
2024-02-28 23:02:38 +0300

    Added search bar

--- src/frontend/pages/main/weatherInfo.tsx
+++ src/frontend/pages/main/weatherInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./weatherInfo.css";
 import { WeatherData } from "../../types/types";
+import SearchBar from "../search/search";
 
 interface WeatherInfoProps {
   weatherInfo: WeatherData | null;
@@ -10,18 +11,25 @@ const WeatherInfo: React.FC<WeatherInfoProps> = ({ weatherInfo }) => {
   console.log(weatherInfo);
 
   return (
-    <div className="weather-info__container">
-      <div className="weather-info__content">
-        <div className="temp">
-          <p className="fact-temp">Temperature: {weatherInfo?.current.temp_c}°C</p>
-          <p className="feelslike-temp">Feels like: {weatherInfo?.current.feelslike_c}°C</p>
+    <div className="weather-info">
+      <div className="weather-info__container">
+        <div className="weather-info__content">
+          <div className="temp">
+            <p className="fact-temp">
+              Temperature: {weatherInfo?.current.temp_c}°C
+            </p>
+            <p className="feelslike-temp">
+              Feels like: {weatherInfo?.current.feelslike_c}°C
+            </p>
+          </div>
+          <p>Description: {weatherInfo?.current.condition.text}</p>
+          <p>Humidity: {weatherInfo?.current.humidity}%</p>
+          <p>
+            Wind: {weatherInfo?.current.wind_mph} m/h{" "}
+            {weatherInfo?.current.wind_dir}
+          </p>
         </div>
-        <p>Description: {weatherInfo?.current.condition.text}</p>
-        <p>Humidity: {weatherInfo?.current.humidity}%</p>
-        <p>
-          Wind: {weatherInfo?.current.wind_mph} m/h{" "}
-          {weatherInfo?.current.wind_dir}
-        </p>
+        <SearchBar />
       </div>
     </div>
   );
---
+++ src/frontend/pages/search/search.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './search.css'; // Подключаем CSS файл
+
+
+const SearchBar: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="search-container" onSubmit={handleSubmit}>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={handleInputChange}
+      />
+      <button className="search-button" type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
