afd7e8af97500ed9b238b352ed35accb35569193
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-04 00:06:20 +0300

    Added get request with param

--- src/frontend/pages/search/search.tsx
+++ src/frontend/pages/search/search.tsx
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
-import './search.css';
-import { useQuery } from 'react-query';
-
-const fetchWeather = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/api/weather-info');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const jsonData = await response.json();
-    return jsonData
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-  }
-};
+import React, { useState } from "react";
+import "./search.css";
+import { useQuery } from "react-query";
 
 const SearchBar: React.FC = () => {
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/weather-info?location=${Location}`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const jsonData = await response.json();
+      return jsonData;
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
 
-  const { isLoading, error, data } = useQuery('weather', fetchWeather);
+  const [Location, setLocation] = useState<string>("");
+  const { isLoading, error, data, refetch } = useQuery("weather", fetchWeather);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    refetch();
+  };
 
   return (
-    <form className="search-form">
+    <form className="search-form" onSubmit={handleSubmit}>
       <input
         className="search-input"
         type="text"
         placeholder="Search..."
+        value={Location}
+        onChange={(e) => setLocation(e.target.value)}
       />
-      <button className="search-button" type="submit">Search</button>
+      <button className="search-button" type="submit">
+        Search
+      </button>
     </form>
   );
 };
