61aefafb7cca0a1b105f1ca16296da11d26f1c40
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-03 23:52:40 +0300

    Created constants in api.js

--- src/backend/api.js
+++ src/backend/api.js
@@ -6,26 +6,30 @@ const app = express();
 app.use(cors());
 const PORT = 3000;
 
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Moscow';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '86da46860bmsh0b86b30185f6acbp19faabjsn1ee73a058bc4',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
+const weatherApiHost = 'weatherapi-com.p.rapidapi.com';
+const weatherApiKey = '86da46860bmsh0b86b30185f6acbp19faabjsn1ee73a058bc4';
 
 app.get('/api/weather-info', async (req, res) => {
-	try {
-		const response = await fetch(url, options);
-		const result = await response.text();
-		res.send(result);
-	} catch (error) {
-		console.error(error);
-		res.status(500).send('ERRRRRRRRRROR');
-	}
+    try {
+        
+        const url = `https://${weatherApiHost}/current.json?q=Moscow`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': weatherApiKey,
+                'X-RapidAPI-Host': weatherApiHost
+            }
+        };
+
+        const response = await fetch(url, options);
+        const result = await response.json();
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(PORT, () => {
-	console.log(`Start on port: ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
